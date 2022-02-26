e07da56a78506cbb8d61fb2e8092f63e473e2abb
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2022-02-26 11:41:04 +0100

    add slider component (WIP)

--- components/Player.tsx
+++ components/Player.tsx
@@ -7,6 +7,7 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 import DefilingText from './DefilingText'
+import ReaderTimeline from './ReaderTimeline'
 
 interface PlayerProps {
   className?: string;
@@ -33,6 +34,7 @@ const Player = ({className}: PlayerProps) => {
   return (
     <div className={`flex flex-col h-max w-full text-white ${className}`}>
       <DefilingText value="artist - track" className="m-auto"/>
+      <ReaderTimeline/>
       <div className="w-full h-24 flex">
         <a onClick={onPreviousClick} className="bg-gray-800 hover:bg-gray-600 rounded-lg m-auto h-12 w-12 flex cursor-pointer">
           <FontAwesomeIcon icon={faArrowLeft} className="m-auto" />
