528dbeb9e18ed2f07fb494fd921762dc42ab290f
abdelillah <aghomari.professionnel@gmail.com>
2022-05-09 04:44:34 +0200

    format

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -3,7 +3,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Spinner = () => {
   return (
-    <FontAwesomeIcon icon={faSpinner} className="mr-2 motion-safe:animate-spin text-grn fa-3x" />
+    <FontAwesomeIcon
+      icon={faSpinner}
+      className="mr-2 motion-safe:animate-spin text-grn fa-3x"
+    />
   );
 };
 export default Spinner;
