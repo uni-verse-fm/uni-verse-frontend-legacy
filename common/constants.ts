f542d4e0911e77b8194fe32dfea57f0c01cc6a5d
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2022-06-21 15:38:30 +0200

    plz next stop wasting my time

--- common/constants.ts
+++ common/constants.ts
@@ -1,5 +1,5 @@
 export const MAX_FILE_SIZE =
-  Number(process.env.NEXT_PUBLIC_MAX_FILE_SIZE) ?? 3_000_000;
+  Number(process.env.NEXT_PUBLIC_MAX_FILE_SIZE) ?? 10_000_000;
 export const MAX_IMAGE_SIZE =
   Number(process.env.NEXT_PUBLIC_MAX_IMAGE_SIZE) ?? 3_000_000;
 
