1c0ec96a84c28aa8d3f682374c9b34324d3ca055
abdelillah <aghomari.professionnel@gmail.com>
2022-06-27 23:19:57 +0200

    strinify env vars

--- next.config.js
+++ next.config.js
@@ -10,14 +10,14 @@ const nextConfig = {
     ],
   },
   env: {
-    STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    UNIVERSE_PRIVATE_KEY: process.env.UNIVERSE_PRIVATE_KEY,
-    UNIVERSE_EMAIL: process.env.UNIVERSE_EMAIL,
-    UNIVERSE_PASSWORD: process.env.UNIVERSE_PASSWORD,
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
-    MINIO_URL: process.env.NEXT_PUBLIC_MINIO_URL,
-    JWT_SECRET: process.env.JWT_SECRET,
+    STRIPE_PUBLISHABLE_KEY: `${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`,
+    STRIPE_SECRET_KEY: `${process.env.STRIPE_SECRET_KEY}`,
+    UNIVERSE_PRIVATE_KEY: `${process.env.UNIVERSE_PRIVATE_KEY}`,
+    UNIVERSE_EMAIL: `${process.env.UNIVERSE_EMAIL}`,
+    UNIVERSE_PASSWORD: `${process.env.UNIVERSE_PASSWORD}`,
+    API_URL: `${process.env.NEXT_PUBLIC_API_URL}`,
+    MINIO_URL: `${process.env.NEXT_PUBLIC_MINIO_URL}`,
+    JWT_SECRET: `${process.env.JWT_SECRET}`,
   },
 };
 
