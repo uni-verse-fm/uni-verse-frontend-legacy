acb44ca0bb174581df56f627635dfe0e89111f99
abdelillah <aghomari.professionnel@gmail.com>
2022-06-27 22:04:01 +0200

    format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,9 @@ import { PlayerProvider } from "../common/contexts/PlayerContext";
 import { SessionProvider } from "next-auth/react";
 import { AxiosProvider } from "../common/contexts/AxiosContext";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
 const options = {
   clientSecret: process.env.STRIPE_CLIENT_SECRET,
 };
