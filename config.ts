c5a1f57c83bfc19523e5dc7bfc6b2a2dbe87cf6e
abdelillah <aghomari.professionnel@gmail.com>
2022-06-28 15:13:22 +0200

    change vars

--- config.ts
+++ config.ts
@@ -1,5 +1,20 @@
-const getEnvironmentVariable = (environmentVariable: string): string => {
-  const unvalidatedEnvironmentVariable = process.env[environmentVariable];
+import getConfig from "next/config";
+
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+
+const getPublicEnvironmentVariable = (environmentVariable: string): string => {
+  const unvalidatedEnvironmentVariable = publicRuntimeConfig[environmentVariable];
+  if (!unvalidatedEnvironmentVariable) {
+    throw new Error(
+      `Couldn't find environment variable: ${environmentVariable}`
+    );
+  } else {
+    return unvalidatedEnvironmentVariable;
+  }
+};
+
+const getPrivateEnvironmentVariable = (environmentVariable: string): string => {
+  const unvalidatedEnvironmentVariable = serverRuntimeConfig[environmentVariable];
   if (!unvalidatedEnvironmentVariable) {
     throw new Error(
       `Couldn't find environment variable: ${environmentVariable}`
@@ -10,13 +25,13 @@ const getEnvironmentVariable = (environmentVariable: string): string => {
 };
 
 export const config = {
-  apiUrl: getEnvironmentVariable("API_URL"),
-  minioUrl: getEnvironmentVariable("MINIO_URL"),
-  stripePubKey: getEnvironmentVariable("UNIVERSE_PUBLIC_KEY"),
-  stripePrivKey: getEnvironmentVariable("STRIPE_SECRET_KEY"),
-  universePrivKey: getEnvironmentVariable("UNIVERSE_ADMIN_PRIVATE_KEY"),
-  maxFileSize: getEnvironmentVariable("NEXT_PUBLIC_MAX_FILE_SIZE"),
-  maxImageSize: getEnvironmentVariable("NEXT_PUBLIC_MAX_IMAGE_SIZE"),
-  univereEmail: getEnvironmentVariable("UNIVERSE_EMAIL"),
-  universePassword: getEnvironmentVariable("UNIVERSE_PASSWORD"),
+  apiUrl: getPublicEnvironmentVariable("API_URL"),
+  minioUrl: getPublicEnvironmentVariable("MINIO_URL"),
+  stripePubKey: getPublicEnvironmentVariable("UNIVERSE_PUBLIC_KEY"),
+  stripePrivKey: getPublicEnvironmentVariable("STRIPE_SECRET_KEY"),
+  maxFileSize: getPublicEnvironmentVariable("MAX_FILE_SIZE"),
+  maxImageSize: getPublicEnvironmentVariable("MAX_IMAGE_SIZE"),
+  universePrivKey: getPrivateEnvironmentVariable("UNIVERSE_ADMIN_PRIVATE_KEY"),
+  univereEmail: getPrivateEnvironmentVariable("UNIVERSE_EMAIL"),
+  universePassword: getPrivateEnvironmentVariable("UNIVERSE_PASSWORD"),
 };
--- next.config.js
+++ next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
     ],
   },
   env: {
+  },
+  serverRuntimeConfig: {
     JWT_SECRET: process.env.JWT_SECRET,
     UNIVERSE_EMAIL: process.env.UNIVERSE_EMAIL,
     UNIVERSE_PASSWORD: process.env.UNIVERSE_PASSWORD,
@@ -17,9 +19,13 @@ const nextConfig = {
     GOOGLE_AUTH_CLIENT_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET,
     GOOGLE_AUTH_CLIENT_ID: process.env.GOOGLE_AUTH_CLIENT_ID,
     GOOGLE_AUTH_CLIENT_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+  },
+  publicRuntimeConfig: {
     API_URL: process.env.NEXT_PUBLIC_API_URL,
     UNIVERSE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     MINIO_URL: process.env.NEXT_PUBLIC_MINIO_URL,
+    MAX_FILE_SIZE: process.env.NEXT_PUBLIC_MAX_FILE_SIZE,
+    MAX_IMAGE_SIZE: process.env.NEXT_PUBLIC_MAX_IMAGE_SIZE,
     UNIVERSE_PRIVATE_KEY: process.env.NEXT_PUBLIC_UNIVERSE_PRIVATE_KEY,
   },
 };
