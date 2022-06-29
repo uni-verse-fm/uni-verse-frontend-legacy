74c7cb916db2573656481782dc4f17d950748058
MEDJOUB NAOUAL <bn.medjoub@gmail.com>
2022-06-29 22:37:20 +0200

    next_config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: [
+        "localhost",
+        "minio.vagahbond.com",
+        `${process.env.NEXT_PUBLIC_MINIO_URL}`,
+        `${process.env.NEXT_PUBLIC_MINIO_URL}/images`,
+        `${process.env.NEXT_PUBLIC_MINIO_URL}/tracks`,
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
