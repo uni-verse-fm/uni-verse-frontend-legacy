7be5fa09d1eb79e3597f739ae294028a5695a072
abdelillah <aghomari.professionnel@gmail.com>
2022-06-30 02:11:07 +0200

    refacto profile

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -8,45 +8,82 @@ import { Session } from "next-auth";
 import Spinner from "../components/Spinner";
 import { Messages } from "../common/constants";
 import { getUserReleases } from "../api/ReleaseAPI";
+import { getUserPlaylists } from "../api/PlaylistAPI";
 
+type ProfileParams = {
+  user: {
+    id: string;
+    username: string;
+    email: string;
+    profilePicture?: string;
+    accountId?: string;
+  };
+  releases: any;
+  playlists: any;
+  isMe: false;
+};
 function Profile() {
   const router = useRouter();
   const {
     query: { id },
   } = router;
 
-  const userQuery = useQuery("user", () =>
-    getUserById(id as string).then((res) => res.data)
+  const userQuery = useQuery(
+    `user-${id}`,
+    () => getUserById(id as string).then((res) => res.data),
+    { enabled: Boolean(id) }
   );
 
-  const releasesQuery = useQuery("myReleases", () =>
-    getUserReleases(id as string).then((res) => res.data)
+  const releasesQuery = useQuery(
+    `releases-${id}`,
+    () => getUserReleases(id as string).then((res) => res.data),
+    { enabled: Boolean(id) }
   );
 
-  const profileParams = (user: any, releases: any) => {
+  const playlistsQuery = useQuery(
+    `playlists-${id}`,
+    () => getUserPlaylists(id as string).then((res) => res.data),
+    { enabled: Boolean(id) }
+  );
+
+  const profileParams = (
+    user: any,
+    releases: any,
+    playlists: any
+  ): ProfileParams => {
     return {
       user: {
         id: user.id,
         username: user.username,
         email: user.email,
+        profilePicture: user.profilePicture,
         accountId: user.accountId,
       },
       releases,
+      playlists,
       isMe: false,
     };
   };
 
-  return userQuery.status === "error" || releasesQuery.status === "error" ? (
+  return "error" in
+    [userQuery.status, releasesQuery.status, playlistsQuery.status] ? (
     <div className="flex justify-center items-center bg-drk w-full h-full">
       <h1 className="text-rd whitespace-nowrap">{Messages.ERROR_LOAD}</h1>
     </div>
-  ) : userQuery.status === "loading" || releasesQuery.status === "loading" ? (
+  ) : "loading" in
+    [userQuery.status, releasesQuery.status, playlistsQuery.status] ? (
     <div className="flex justify-center items-center bg-drk w-full h-full">
       <Spinner />
     </div>
   ) : (
     <div className="bg-drk w-full h-full ">
-      <ProfileScreen {...profileParams(userQuery.data, releasesQuery.data)} />
+      <ProfileScreen
+        {...profileParams(
+          userQuery.data,
+          releasesQuery.data,
+          playlistsQuery.data
+        )}
+      />
     </div>
   );
 }
