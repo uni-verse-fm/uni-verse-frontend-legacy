162c15ee16c3568551f8e0136bbb39f4f8320f6f
abdelillah <aghomari.professionnel@gmail.com>
2022-07-03 13:27:23 +0200

    fix artists search tab system

--- pages/UserRelease.tsx
+++ pages/UserRelease.tsx
@@ -6,6 +6,10 @@ import { getSession, GetSessionParams } from "next-auth/react";
 import { Session } from "next-auth";
 import { adminLogin } from "../api/AdminAPI";
 import { ILogin } from "../common/types";
+import { useQuery } from "react-query";
+import { getReleaseById } from "../api/ReleaseAPI";
+import Spinner from "../components/Spinner";
+import { Messages } from "../common/constants";
 
 function UserRelease() {
   const router = useRouter();
@@ -13,17 +17,34 @@ function UserRelease() {
     query: { id },
   } = router;
 
+  const { status, data } = useQuery(
+    `release-${id}`,
+    () => getReleaseById(id as string).then((res) => res.data),
+    { enabled: Boolean(!!id) }
+  );
   return (
     <div className="bg-drk w-full h-full flex flex-col overflow-y-scroll overflow-x-hidden p-10">
       <div className="text-start justify-start items-start w-full h-full ">
         <h1 className="text-xl font-bold not-italic text-grn  mb-5 mt-10">
           Release
         </h1>
-        <ArtistRelease
-          index={id}
-          handleClosePlaylistContent="{handleClosePlaylistContent}"
-          enableChange="false"
-        />
+        <div className="Global bg-grey w-full h-full flex flex-col">
+          {status === "loading" ? (
+            <div className="flex justify-center items-center mt-10">
+              <Spinner />
+            </div>
+          ) : status === "error" ? (
+            <div className="flex justify-center items-center mt-10">
+              <h1 className="text-rd whitespace-nowrap">
+                {Messages.ERROR_LOAD}
+              </h1>
+            </div>
+          ) : (
+            <ArtistRelease
+              release={data}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
