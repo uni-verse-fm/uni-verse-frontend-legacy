706288472fd512fe60ce79c82a331d6ad0505e3a
MEDJOUB NAOUAL <bn.medjoub@gmail.com>
2022-07-05 00:35:07 +0200

    terms

--- pages/LicencePage.tsx
+++ pages/LicencePage.tsx
@@ -2,9 +2,55 @@ function LicencePage() {
     return (
         <div className="bg-drk w-full h-full flex flex-col overflow-y-scroll overflow-x-hidden p-10">
         <div className="text-start justify-start items-start w-full h-full ">
-          <h1 className="text-xl font-bold not-italic text-grn  mb-5 mt-10">
-           General terms and conditions of use:
-        </h1>
+          <h2 className="text-xl font-bold not-italic text-grn  mb-5 mt-10">
+           General terms and conditions of use :
+        </h2>
+        <p className="text-md  not-italic text-wht  mb-5  ">
+        Welcome to Uni-verse. Uni-verse is more than just a platform for sharing and listening to music. It's a true social network and resource hub for music producers.<br/>
+
+Our platform goes beyond the possibility of publishing, listening, commenting, grouping music into playlists, and consulting its statistics. <br/>
+In addition to these features, it is possible to shazam, thanks to our mobile application in particular, the music published on the platform. <br/>
+
+A system for sharing resources (samples, presets, etc.) specially adapted to the needs of producers has also been set up :<br/>
+ it is possible to share their resources and use those of others. <br/>
+Sharing is governed by a specific payment system: <br/>it is possible to offer resources for free, at a fixed price, or even to make them available<br/>
+ to all other users who have already made donations (via the platform) of a value cumulative greater than or equal to a certain amount. 
+So, mutual aid between artists is encouraged, as well as the sharing of resources, via a centralized platform, standardized for the first time, and adapted to the needs of producers.
+
+An anti-plagiarism system been set up using the audio recognition algorithm (fingerprinting), to trace resources that are reposted. So, a notification is sent to the first Internet user, allowing him to accept or refuse the use of his resource.
+
+<br/>
+        </p>
+
+        <p className="text-md  not-italic text-wht  mb-5  ">
+        In order to ensure a good to ensure a good atmosphere and collaboration between the community  members, it is requested to respect the following general rules :<br/>
+        </p>
+
+
+        
+        <p className="text-md  not-italic text-wht  mb-5  ">
+        In order to ensure a good to ensure a good atmosphere and collaboration between the community  members, it is requested to respect the following general rules :<br/>
+        </p>
+        <ul>
+       
+
+        <li className="text-md  not-italic text-wht  mb-5  ml-10" >
+             - Respect: No insults or intolerant remarks in comments about an individual or a group of individuals.
+             </li>
+             <li className="text-md  not-italic text-wht  mb-5  ml-10" >
+             - Ensure decorum in the music and in images content used.
+             </li>
+             <li className="text-md  not-italic text-wht  mb-5  ml-10" >
+             - Support and sharing resources between artists is highly recommended.
+             </li>
+        </ul>
+
+
+
+        <p className="text-md  not-italic text-wht  mb-5 ">
+        Any violation of rules will result in a warning and may lead to banishment from the community.<br/>
+        </p>
+
 
 
         </div>
