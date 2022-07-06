dadc6ea9070985c1265cd08470eedfac4a811887
abdelillah <aghomari.professionnel@gmail.com>
2022-07-06 14:20:36 +0200

    format

--- api/FeatRequestsAPI .ts
+++ api/FeatRequestsAPI .ts
@@ -4,14 +4,10 @@ import { Endoints } from "../common/types";
 const FeatRequestsEndpoint = Endoints.FeatRequests;
 
 const findSentRequestId = () =>
-  axiosClient
-    .get(`${FeatRequestsEndpoint}/sent`)
-    .then((res) => res.data);
+  axiosClient.get(`${FeatRequestsEndpoint}/sent`).then((res) => res.data);
 
 const findReceivedRequestId = () =>
-  axiosClient
-    .get(`${FeatRequestsEndpoint}/received`)
-    .then((res) => res.data);
+  axiosClient.get(`${FeatRequestsEndpoint}/received`).then((res) => res.data);
 
 const acceptRequest = (requestId: string) =>
   axiosClient.patch(`${FeatRequestsEndpoint}/accept/${requestId}`);
